5a2275b390f8056edf0cc6d29765f13bf8aaf9bf
Viktor Brui <viktor-brui@mail.ru>
2023-07-21 17:02:25 +0400

    deploy project

---
+++ src/pages/AddPost/index.jsx
@@ -0,0 +1,174 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import SimpleMDE from "react-simplemde-editor";
+
+import "easymde/dist/easymde.min.css";
+import styles from "./AddPost.module.scss";
+import { useSelector } from "react-redux";
+import { selectIsAuth } from "../../redux/slices/auth";
+import { useNavigate, Navigate, useParams } from "react-router-dom";
+import axios from "../../axios";
+
+export const AddPost = () => {
+  const { id } = useParams();
+  const isAuth = useSelector(selectIsAuth);
+  const navigate = useNavigate();
+
+  // const imageUrl = "";
+  const [isLoading, setLoading] = React.useState(false);
+  const [text, setText] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [tags, setTags] = React.useState("");
+  const [imageUrl, setImageUrl] = React.useState("");
+  const inputFileRef = React.useRef(null);
+
+  const isEditing = Boolean(id);
+  const handleChangeFile = async (event) => {
+    try {
+      const formData = new FormData();
+      const file = event.target.files[0];
+      formData.append("image", file);
+      const { data } = await axios.post("upload", formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert("ошибка загруки файла");
+    }
+  };
+
+  const onClickRemoveImage = () => {
+    setImageUrl("");
+  };
+
+  const onChange = React.useCallback((value) => {
+    setText(value);
+  }, []);
+
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+
+      const fields = {
+        title,
+        text,
+        imageUrl,
+        tags,
+      };
+
+      const { data } = isEditing
+        ? await axios.patch(`/posts/${id}`, fields)
+        : await axios.post("/posts", fields);
+
+      const _id = isEditing ? id : data._id;
+
+      navigate(`/posts/${_id}`);
+    } catch (err) {
+      console.warn(err);
+      alert("Ошибка при создании статьи");
+    }
+  };
+
+  React.useEffect(() => {
+    if (id) {
+      axios
+        .post(`/posts/${id}`)
+        .then(({ data }) => {
+          setTitle(data.title);
+          setText(data.text);
+          setTags(data.tags.join(","));
+          setImageUrl(data.imageUrl);
+        })
+        .catch((err) => {
+          console.warn(err);
+          alert("Ошибка при получении статьи");
+        });
+    }
+  }, []);
+
+  const options = React.useMemo(
+    () => ({
+      spellChecker: false,
+      maxHeight: "400px",
+      autofocus: true,
+      placeholder: "Введите текст...",
+      status: false,
+      autosave: {
+        enabled: true,
+        delay: 1000,
+      },
+    }),
+    []
+  );
+  const isToken = window.localStorage.getItem("token");
+  if (!isToken && !isAuth) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <Paper style={{ padding: 30 }}>
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
+        Загрузить превью
+      </Button>
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
+      {imageUrl && (
+        <>
+          <Button
+            variant="contained"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Удалить
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4444${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
+      )}
+      <br />
+      <br />
+      <TextField
+        classes={{ root: styles.title }}
+        variant="standard"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Заголовок статьи..."
+        fullWidth
+      />
+      <TextField
+        classes={{ root: styles.tags }}
+        variant="standard"
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        placeholder="Тэги"
+        fullWidth
+      />
+      <SimpleMDE
+        className={styles.editor}
+        value={text}
+        onChange={onChange}
+        options={options}
+      />
+      <div className={styles.buttons}>
+        <Button onClick={onSubmit} size="large" variant="contained">
+          {isEditing ? "Сохранить" : "Опубликовать"}
+        </Button>
+        <a href="/">
+          <Button size="large">Отмена</Button>
+        </a>
+      </div>
+    </Paper>
+  );
+};
---
+++ src/pages/Registration/index.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+
+import styles from "./Login.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRegister, selectIsAuth } from "../../redux/slices/auth";
+import { useForm } from "react-hook-form";
+import { Navigate } from "react-router-dom";
+
+export const Registration = () => {
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      fullname: "",
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchRegister(values));
+    if (!data.payload) {
+      alert("Не удалось зарегистрироваться");
+    } else {
+      console.log(values);
+      window.localStorage.setItem("token", data.payload.token);
+    }
+  };
+
+  if (isAuth) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <Paper classes={{ root: styles.root }}>
+      <Typography classes={{ root: styles.title }} variant="h5">
+        Создание аккаунта
+      </Typography>
+      <div className={styles.avatar}>
+        <Avatar sx={{ width: 100, height: 100 }} />
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="Полное имя"
+          error={Boolean(errors.fullname?.message)}
+          helperText={errors.fullname?.message}
+          {...register("fullname", { required: "Укажите имя" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          type="email"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          {...register("email", { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          {...register("password", { required: "Укажите пароль" })}
+          fullWidth
+        />
+        <Button
+          disabled={!isValid}
+          type={"submit"}
+          size="large"
+          variant="contained"
+          fullWidth
+        >
+          Зарегистрироваться
+        </Button>
+      </form>
+    </Paper>
+  );
+};
